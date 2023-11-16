c5f1ce4aecec89667b13236a13db91de0ab96ef6
rlunatrepczynski <crenee72@hotmail.com>
2023-11-15 22:16:39 -0700

    Update scripts.js
    Added jQuery for exercise 1.9.

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function newItem() {
+
+    //add new element
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        let list = $('#list');
+        list.appendChild(li);
+    }
+
+    //2. Crossing out an item from the list of items:
+    function crossOut() {
+        li.toggleClass('strike');
+    }
+
+    li.on('dblclick', crossOut);
+
+    //3(i). Adding the delete button "X": 
+    let crossOutButton = $('<button></button>');
+    crossOutButton.append(document.createTextNode("X"));
+    li.append(crossOutButton);
+
+    crossOutButton.on("click", deleteListItem);
+    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+    function deleteListItem() {
+        li.addClass('delete');
+    }
+    // 4. Reordering the items: 
+    $('#list').sortable();
+
+}
